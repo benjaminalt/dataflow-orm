a59dae1b63487c285e6d3a0d8f20fb7608be6307
Benjamin Alt <benjamin_alt@outlook.com>
2018-10-03 22:06:43 +0200

    Example/test print visitor

--- draft/main.cpp
+++ draft/main.cpp
@@ -1,7 +1,13 @@
 #include <dataflow/Person.h>
 #include <dataflow/Order.h>
+#include <dataflow/PrintVisitor.h>
 
 int main()
 {
-    return 0;    
+    dataflow::Person person(0, "Rogers", "Steve", 22);
+    dataflow::Order order(0, 10, person.personID().get());
+    dataflow::PrintVisitor printVisitor;
+    person.accept(printVisitor);
+    order.accept(printVisitor);
+    return 0;
 }
